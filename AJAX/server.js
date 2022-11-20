bddcc73021f45209b4bc4c4f5277eed218a604e5
cui-homestudy <cuiqiyuan1989@gmail.com>
2022-11-19 22:44:41 -0600

    AJAX, get&post&all, JSON,

--- AJAX/server.js
+++ AJAX/server.js
@@ -12,9 +12,42 @@ app.get('/server',(rquest,response)=>{
     // 设置响应头， 设置允许跨域（Access-Control-Allow-Origin）
     response.setHeader('Access-Control-Allow-Origin','*');
     // 设置响应体
-    response.send('Hello Ajax');
+    response.send('Hello Ajax GET');
 
 });
+
+app.post('/server',(rquest,response)=>{
+    // 设置响应头， 设置允许跨域（Access-Control-Allow-Origin）
+    response.setHeader('Access-Control-Allow-Origin','*');
+    // 设置响应体
+    response.send('Hello Ajax POST');
+
+});
+
+app.all('/server',(rquest,response)=>{
+    // 设置响应头， 设置允许跨域（Access-Control-Allow-Origin）
+    response.setHeader('Access-Control-Allow-Origin','*');
+    //设置接收自定义响应头
+    response.setHeader('Access-Control-Allow-Headers','*');
+    // 设置响应体
+    response.send('Hello Ajax All');
+});
+
+app.all('/json-server',(rquest,response)=>{
+    // 设置响应头， 设置允许跨域（Access-Control-Allow-Origin）
+    response.setHeader('Access-Control-Allow-Origin','*');
+    //设置接收自定义响应头
+    response.setHeader('Access-Control-Allow-Headers','*');
+    //相应一个数据
+    const data = {
+        name:'HAOHAO'
+    }
+    //需要对数据进行字符串转换
+    let str = JSON.stringify(data)
+    // 设置响应体
+    response.send(str);
+});
+
 
 // 4.监听端口启动服务
 app.listen (8000,()=>{
